fca777ae4d2616789f53b6f40f3fdec9dc53494f
Alexander Wald <mathal@rcn.com>
2015-04-13 22:55:55 -0400

    Modified the following classes: BlockView - added a static 2D array to hold the 18 block icons

--- src/forms/BlockView.java
+++ src/forms/BlockView.java
@@ -6,35 +6,63 @@ import javax.swing.JButton;
 import entities.Block;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionListener;
+import javax.swing.JLabel;
 
 public class BlockView extends JPanel
 {
+	static final int NORMAL_COLOR = 0xFFFF00;
+	static final int SELECTED_COLOR = 0xFF0000;
+	
+	static final String[][] icons = new String[6][3];
+	
+	static {
+
+		for (int value = 1; value <= 6; value++) {
+			for (int mult = 1; mult <= 3; mult++) {
+				icons[value - 1][mult - 1] = new String(value + " x" + mult);
+			}
+		}
+		System.out.println("static" + icons[0][1]);
+	}
+	
+	
 	Block block;
-	JButton button;
-	//Icon icon;
+	JLabel label;
+	String icon;
 
 	public BlockView(Block block) {
 		super();
 		this.block = block;
 		setLayout(new BorderLayout(0, 0));
-		add(getButton(), BorderLayout.CENTER);
-//		button.setIcon(icon.getImage());
+		add(getLabel(), BorderLayout.CENTER);
+		update();
 		
 	}
 	
-	JButton getButton() {
-		if (button == null) {
-			button = new JButton("");
-		}
-		return button;
-	}
-	
 	public void addActionListener(ActionListener al) {
-		button.addActionListener(al);
+		this.addActionListener(al);
 	}
 	
 	public void update() {
-		button.setText(block.getNumber() + "\n\n" + "x" + block.getMultiplier());
+//		if (block == null) {
+//			this.setVisible(false);
+//			return;
+//		}
+//		else
+//			this.setVisible(true);
+		
+		if (block == null)
+			getLabel().setText("");
+		else
+			getLabel().setText(icons[block.getValue() - 1][block.getMultiplier() - 1]);
+	}
+	
+	JLabel getLabel() {
+		if (label == null) {
+			label = new JLabel("");
+		}
+		return label;
 	}
 }
