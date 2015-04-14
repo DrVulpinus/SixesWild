2ce582e76e4bf3033a40dcdc11c05261459a25de
DrVulpinus <siganus.vulpinus@live.com>
2015-04-13 22:16:22 -0400

    Begin Filling in SelectViews

--- src/forms/LevelSelectView.java
+++ src/forms/LevelSelectView.java
@@ -1,5 +1,18 @@
 package forms;
 
+import java.util.ArrayList;
+
+import entities.Level;
+
 public class LevelSelectView extends SelectView {
 
+	ArrayList<Level> levels = new ArrayList<Level>();
+	@Override
+	void loadItems() {
+		for (Level level : levels) {
+			
+		}
+		
+	}
+	
 }
--- src/forms/SelectView.java
+++ src/forms/SelectView.java
@@ -1,7 +1,28 @@
 package forms;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public abstract class SelectView extends JPanel {
+import java.awt.BorderLayout;
+import java.util.ArrayList;
 
+import javax.swing.JScrollPane;
+
+import net.miginfocom.swing.MigLayout;
+
+public abstract class SelectView extends JPanel {
+	private JScrollPane scrollPane;
+	ArrayList<JLabel> items = new ArrayList<JLabel>();
+	public SelectView() {
+		setLayout(new MigLayout("", "[450px]", "[300px]"));
+		add(getScrollPane(), "cell 0 0,grow");
+	}
+
+	JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+		}
+		return scrollPane;
+	}
+	abstract void loadItems();
 }
