672435b4057cd854970bfa390f1272c4618b84e8
DrVulpinus <siganus.vulpinus@live.com>
2015-04-15 01:14:59 -0400

    Updated Main Menu View for controls Updated Start Program

--- src/forms/MainMenuView.java
+++ src/forms/MainMenuView.java
@@ -12,41 +12,40 @@ import javax.swing.Action;
 
 import org.eclipse.wb.swing.FocusTraversalOnArray;
 
+
+
 import java.awt.Component;
 import java.awt.Cursor;
-
 import java.awt.Dimension;
 
 import net.miginfocom.swing.MigLayout;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenuView extends JFrame{
 
 
 	public JButton btnLevels = new JButton("Levels");	
 	public JButton btnAchievements = new JButton("Achievements");
+	public LevelSelectView lvlSelectView = new LevelSelectView();
 	/**
 	 * Create the panel.
 	 */
 	public MainMenuView() {
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent arg0) {
-				
-			}
-			@Override
-			public void windowClosed(WindowEvent e) {
-				MainMenuView.this.dispose();
-			}
-		});
+		
 		getContentPane().setMinimumSize(new Dimension(500, 500));
 		setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		setBackground(UIManager.getColor("ToolTip.background"));
 		
 		
 		getContentPane().setLayout(new MigLayout("", "[grow][117px][117px][grow]", "[grow][40px,center][grow]"));
+		btnLevels.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setContentPane(lvlSelectView);
+			}
+		});
 		getContentPane().add(btnLevels, "cell 1 1,grow");
 		getContentPane().add(btnAchievements, "cell 2 1,grow");
 		setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{btnLevels, btnAchievements}));
