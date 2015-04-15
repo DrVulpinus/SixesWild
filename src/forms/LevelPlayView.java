1c8fca652303199e43d7c06a3ecf578f61805876
schamberlain1110 <schamberlain1110@gmail.com>
2015-04-14 23:55:54 -0400

    Updates LevelPlayView to display stats.

--- src/forms/LevelPlayView.java
+++ src/forms/LevelPlayView.java
@@ -1,12 +1,35 @@
 package forms;
 
+import java.awt.EventQueue;
+
 import javax.swing.JPanel;
+
+import src.SixesWildWindow;
 public class LevelPlayView extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				
+				try {
+					SixesWildWindow stats = new SixesWildWindow();
+					stats.setVisible(true);
+					//Thread.sleep(10000);
+					//frame.setVisible(false);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			
+			}
+		});
+	}
 	public LevelPlayView() {
+		
+		SixesWildWindow sixesWildWindow = new SixesWildWindow();
+		add(sixesWildWindow);
 
 	}
 
