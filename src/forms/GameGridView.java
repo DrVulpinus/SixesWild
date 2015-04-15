14f3dc7c5c78dea8c681bb8369f853ef0f845c1b
schamberlain1110 <schamberlain1110@gmail.com>
2015-04-15 00:50:14 -0400

    Implemented the GameGridView and ShowGameGridView class.

--- src/forms/GameGridView.java
+++ src/forms/GameGridView.java
@@ -1,14 +1,76 @@
 package forms;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 
-public class GameGridView extends JPanel {
+import javax.swing.JButton;
+import javax.swing.JComponent;
 
-	/**
-	 * Create the panel.
-	 */
-	public GameGridView() {
+public class GameGridView extends JComponent {
 
-	}
-
+	
+Color color = new Color(0,0,0); 
+double currentXVal= 0;
+double currentYVal = 0;
+int yNumLines =9;
+int xNumLines =9;
+double xIncrement = 0;
+double yIncrement = 0;
+	
+	public void paintComponent(Graphics g) {
+       Graphics2D g2 = (Graphics2D) g;
+       
+       g2.setColor(color);
+       
+       double leftX = 100;
+       double topY = 100;
+       double dimension= Math.min(getWidth(), getHeight());
+       
+       double width = dimension;
+       double height = dimension;
+       double linesX=0;
+       double lineY=0.0;
+       double linesY=0;
+       double lineX=0.0;
+       
+       
+       Rectangle2D rect = new Rectangle2D.Double(0, 0, width, height);
+       g2.draw(rect);
+       
+       
+       
+      //g2.draw(new Line2D.Double(rect.getX(), rect.getY(), rect.getX()+ rect.getWidth(), rect.getY() + rect.getHeight()));
+       while(linesX<xNumLines){
+    	   
+       
+       g2.draw(new Line2D.Double(rect.getX(), lineY, rect.getX()+ rect.getWidth(), lineY
+    		   ));
+       linesX++;
+      xIncrement= rect.getHeight()/xNumLines;
+       lineY+=rect.getHeight()/xNumLines;
+       //System.out.println(lineY);
+       
+       }
+       
+       while (linesY<yNumLines){
+    	   g2.draw(new Line2D.Double(lineX, rect.getY(), lineX, rect.getY()+ rect.getHeight()
+        		   ));  
+    	   linesY++;
+    	   yIncrement= rect.getWidth()/yNumLines;
+    	   
+    	   lineX+=rect.getWidth()/yNumLines;
+       }
+       
 }
+	
+	
+}
+
+
