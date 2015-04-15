568ae5c06e00e28f38708bc6400a5a6ef0b6b3bb
tiniminimini <migcornbread@gmail.com>
2015-04-15 16:33:31 -0400

    Added Splash Screen and transition to Main Menu and Level Builder to the Level Editor

--- LevelBuilder/forms/MenuView.java
+++ LevelBuilder/forms/MenuView.java
@@ -1,14 +1,24 @@
 package forms;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class MenuView extends JPanel {
+	private JButton btnCreateNewLevel;
 
 	/**
 	 * Create the panel.
 	 */
 	public MenuView() {
+		setLayout(null);
+		add(getBtnCreateNewLevel());
 
 	}
-
+	JButton getBtnCreateNewLevel() {
+		if (btnCreateNewLevel == null) {
+			btnCreateNewLevel = new JButton("Create New Level");
+			btnCreateNewLevel.setBounds(69, 147, 302, 23);
+		}
+		return btnCreateNewLevel;
+	}
 }
