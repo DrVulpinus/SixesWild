df5a42fc9b2e9a0b0bcbb2931e6544aed2b70457
tiniminimini <migcornbread@gmail.com>
2015-04-16 20:50:10 -0400

    Working on Achievement Menu, updated MainFrame to have 200 x 200

--- src/forms/AchievementSelectView.java
+++ src/forms/AchievementSelectView.java
@@ -1,6 +1,21 @@
 package forms;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
 
 public class AchievementSelectView extends SelectView{
+	private JLabel lblLvlIcon;
+	private JLabel lblLvlNumber;
+	private JLabel lblStar;
+	private JLabel lblStar_1;
+	private JLabel lblStar_2;
+	public AchievementSelectView() {
+		setLayout(new MigLayout("", "[][][][][][][][]", "[]"));
+		add(getLblLvlIcon(), "cell 0 0");
+		add(getLblLvlNumber(), "cell 1 0");
+		add(getLblStar(), "cell 3 0");
+		add(getLblStar_1(), "cell 5 0");
+		add(getLblStar_2(), "cell 7 0");
+	}
 
 	@Override
 	void loadItems() {
@@ -8,4 +23,34 @@ public class AchievementSelectView extends SelectView{
 		
 	}
 
+	private JLabel getLblLvlIcon() {
+		if (lblLvlIcon == null) {
+			lblLvlIcon = new JLabel("Lvl Icon");
+		}
+		return lblLvlIcon;
+	}
+	private JLabel getLblLvlNumber() {
+		if (lblLvlNumber == null) {
+			lblLvlNumber = new JLabel("Lvl Number");
+		}
+		return lblLvlNumber;
+	}
+	private JLabel getLblStar() {
+		if (lblStar == null) {
+			lblStar = new JLabel("Star 1");
+		}
+		return lblStar;
+	}
+	private JLabel getLblStar_1() {
+		if (lblStar_1 == null) {
+			lblStar_1 = new JLabel("Star 2");
+		}
+		return lblStar_1;
+	}
+	private JLabel getLblStar_2() {
+		if (lblStar_2 == null) {
+			lblStar_2 = new JLabel("Star 3");
+		}
+		return lblStar_2;
+	}
 }
