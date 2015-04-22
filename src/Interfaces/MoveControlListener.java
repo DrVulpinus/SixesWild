69b537d171e7380c638873f59828090ed956792e
schamberlain1110 <schamberlain1110@gmail.com>
2015-04-22 14:31:46 -0400

    Finished MoveControlListener interface

---
+++ src/Interfaces/MoveControlListener.java
@@ -0,0 +1,16 @@
+package Interfaces;
+
+import java.util.EventListener;
+
+
+import forms.SquareView;
+
+public interface MoveControlListener  extends EventListener{
+
+	void performMove();
+
+	void startMove();
+	void selectBlock(SquareView sV);
+	void endMove() ;
+
+}
