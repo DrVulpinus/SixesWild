695e891401427bcada51b2f80a7a7f73a78523e5
Alexander Wald <mathal@rcn.com>
2015-04-22 17:11:57 -0400

    updated SelectMoveController

--- src/controllers/SelectMoveController.java
+++ src/controllers/SelectMoveController.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 import entities.Move;
+import forms.LevelPlayView;
 import forms.SpecialMoveView;
 
 public class SelectMoveController implements ActionListener {
@@ -15,37 +16,41 @@ JButton swap;
 JButton remove;
 JButton reset;
 
-		public SelectMoveController( JButton swap, JButton remove , JButton reset ) {
+		public SelectMoveController(LevelPlayView levelPlayView) {
 		
+				this.swap = levelPlayView.getSpecialMoveView().getSwapSquareButton();
+				this.remove = levelPlayView.getSpecialMoveView().getRemoveSquareButton();
+				this.reset = levelPlayView.getSpecialMoveView().getResetBoardButton();
 		    
-		        this.swap = new JButton("1");
-		        this.swap.addActionListener(this);
-		        this.swap = swap;
-		        
-		        this.remove = new JButton("2");
-		        this.remove.addActionListener(this);
-		        this.remove = remove;
-		        
-		        this.reset = new JButton("3");
-		        this.reset.addActionListener(this);
-		        this.remove = remove;
+//		        this.swap = new JButton("1");
+//		        this.swap.addActionListener(this);
+//		        this.swap = swap;
+//		        
+//		        this.remove = new JButton("2");
+//		        this.remove.addActionListener(this);
+//		        this.remove = remove;
+//		        
+//		        this.reset = new JButton("3");
+//		        this.reset.addActionListener(this);
+//		        this.remove = remove;
 		    }
 
-		public void mousePressed (java.awt.event.MouseEvent me) {
-
-				
-			switch(button){
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
 			
-			
-			
+			if (e.getSource() == swap){
 			
 			}
+			
+			if (e.getSource() == remove){
+				
+			}
+			
+			if (e.getSource() == reset){
+				
+			}	
 		}
-
-
-	
-
-		
 	}	
 	
 	
--- src/entities/LevelPlayState.java
+++ src/entities/LevelPlayState.java
@@ -2,13 +2,16 @@ package entities;
 
 public class LevelPlayState {
 
-	static Class<Move> move;
+	public static int MOVE_REGULAR = 0;
+	public static int MOVE_SWAP = 1;
+	public static int MOVE_REMOVE = 2;
+	public static int MOVE_RESET = 3;
 	
-	public static void setMove(Class<Move> move) {
+	public static void setSelectedMove(Class<Move> move) {
 		LevelPlayState.move = move;
 	}
 	
-	public static Class<Move> getMove() {
+	public static Class<Move> getSelectedMove() {
 		return move;
 	}
 }
