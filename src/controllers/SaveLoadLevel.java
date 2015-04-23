093c352ec5aa6da1a504e35dd230838406d86c20
DrVulpinus <siganus.vulpinus@live.com>
2015-04-22 23:07:23 -0400

    I TESTED SOMETHING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/controllers/SaveLoadLevel.java
+++ src/controllers/SaveLoadLevel.java
@@ -1,32 +1,48 @@
 package controllers;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 import entities.Level;
 
 public final class SaveLoadLevel {
 private XStream xstream = null;
-private FileWriter writer;
-private FileReader reader;
-public SaveLoadLevel(String levelName){
-	xstream = new XStream();
-	 try {
-		writer = new FileWriter(levelName + ".xml");
-		reader = new FileReader(levelName + ".xml");
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+private File saveFile;
+private SaveLoadLevel(){
+	xstream = new XStream(new StaxDriver());
+	xstream.ignoreUnknownElements();
+	
+	
 }
-	public void saveLevel(Level lvl){
+public static SaveLoadLevel getInstance(){
+	return new SaveLoadLevel();
+}
+	public void saveLevel(Level lvl, String lvlName){
+		saveFile  = new File(lvlName + ".xml");
+		
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(saveFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		xstream.toXML(lvl, writer);
+		try {
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public Level getLevel(){
-		return (Level) xstream.fromXML(reader);
+		return (Level) xstream.fromXML(saveFile);
 	}
 	
 }
