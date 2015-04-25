2c0ed9cd47e4d032cab9c28d3634482971c7ba7c
schamberlain1110 <schamberlain1110@gmail.com>
2015-04-25 17:44:49 -0400

    Finished ToolSelectionController and modified  the getToolSelector in BuildLevelView so that it didn't use strings.

--- LevelBuilder/builder_controllers/ToolSelectionController.java
+++ LevelBuilder/builder_controllers/ToolSelectionController.java
@@ -1,16 +1,36 @@
 package builder_controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+
 import forms.BuildLevelView;
 import builder_entities.LevelBuildState;
 
-public class ToolSelectionController {
+public class ToolSelectionController implements ActionListener  {
 	
 	BuildLevelView buildLevelView;
 	LevelBuildState buildState;
+	JComboBox toolSelect;
+	
 	
 	public ToolSelectionController(BuildLevelView buildLevelView, LevelBuildState buildState) {
 		this.buildLevelView = buildLevelView;
 		this.buildState = buildState;
+		toolSelect = buildLevelView.getToolSelector();
+		this.toolSelect.addActionListener(this);
+	}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			
+				buildState.setSelectedTool(((JComboBox)e.getSource()).getSelectedIndex());
+			
+			
+				
+
+		
 		
 	}
 	
