6b88d1acbedfcda8da0461b391f2b8db469a2b22
DrVulpinus <siganus.vulpinus@live.com>
2015-04-27 21:06:03 -0400

    Cleaning up GameGridView

--- src/forms/GameGridView.java
+++ src/forms/GameGridView.java
@@ -1,14 +1,11 @@
 package forms;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
-import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import Interfaces.MoveControlListener;
