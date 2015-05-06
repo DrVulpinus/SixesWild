7c30f1a4cd6464f0ad1b55dd1eed0b75ccf1e5ae
Alexander Wald <mathal@rcn.com>
2015-05-06 04:10:50 -0400

    added documentation to StockLevels

--- src/entities/StockLevels.java
+++ src/entities/StockLevels.java
@@ -4,33 +4,74 @@ import java.util.ArrayList;
 
 import src.PuzzleStats;
 
-
+/**
+ * StockLevels provides methods for making the built in set of 16 levels
+ * and adds the levels to a list.
+ * 
+ */
 
 public class StockLevels extends ArrayList<Level>{
+	
+	/** The list of 16 built in levels */
 	private ArrayList<Level> levels; 
+	
+	/** Puzzle Level 1 */
 	Level puzzle1;
+	
+	/** Puzzle Level 2 */
 	Level puzzle2;
+	
+	/** Puzzle Level 3 */
 	Level puzzle3;
+	
+	/** Puzzle Level 4 */
 	Level puzzle4;
+	
+	/** Elimination Level 1 */
 	Level elimination1;
+	
+	/** Elimination Level 2 */
 	Level elimination2;
+	
+	/** Elimination Level 3 */
 	Level elimination3;
+	
+	/** Elimination Level 4 */
 	Level elimination4;
+	
+	/** Release Level 1 */
 	Level release1;
+	
+	/** Release Level 2 */
 	Level release2;
+	
+	/** Release Level 3 */
 	Level release3;
+	
+	/** Release Level 4 */
 	Level release4;
+	
+	/** Lightning Level 1 */
 	Level lightning1;
+	
+	/** Lightning Level 2 */
 	Level lightning2;
+	
+	/** Lightning Level 3 */
 	Level lightning3;
+	
+	/** Lightning Level 4 */
 	Level lightning4;
-	Square square1;
-	Square square2;
-	Square square3;
-	Square square4;
-	Square square5;
-	Square square6;
+	
+	
+	
+	/** The StockLevels singleton instance */
 	public static StockLevels stockLevel;
+	
+	/**
+	 * Gets the instance of the StockLevels
+	 * @return the instance of the StockLevels
+	 */
 	public static StockLevels getInstance(){
 		if (stockLevel == null){
 			stockLevel = new StockLevels();
@@ -42,7 +83,9 @@ public class StockLevels extends ArrayList<Level>{
 
 
 
-
+	/** 
+	 * Constructs a StockLevels and initializes the 16 levels
+	 */
 	public StockLevels() {
 		this.setLevels(new ArrayList<Level>());
 		this.puzzle1 = new Level(LevelType.PUZZLE);
@@ -64,12 +107,12 @@ public class StockLevels extends ArrayList<Level>{
 		this.lightning2 = new Level(LevelType.LIGHTNING);
 		this.lightning3 = new Level(LevelType.LIGHTNING);
 		this.lightning4 = new Level(LevelType.LIGHTNING);
-
-		this.square1 = new Square();
-		
 	}
 
-
+	/**
+	 * Generates Puzzle level 1
+	 * @return Puzzle Level 1
+	 */
 	public Level generatePuzzle1(){
 		puzzle1.setName("Puzzle1");
 		Probability p = new Probability();
@@ -89,7 +132,10 @@ public class StockLevels extends ArrayList<Level>{
 	}
 
 
-
+	/**
+	 * Generates Puzzle level 2
+	 * @return Puzzle Level 2
+	 */
 	public Level generatePuzzle2(){
 		puzzle2.setName("Puzzle2");
 		Probability p = new Probability();
@@ -105,7 +151,11 @@ public class StockLevels extends ArrayList<Level>{
 		}
 		return puzzle2;
 	}
-
+	
+	/**
+	 * Generates Puzzle level 3
+	 * @return Puzzle Level 3
+	 */
 	public Level generatePuzzle3(){
 		puzzle3.setName("Puzzle3");
 		Probability p = new Probability();
@@ -122,7 +172,11 @@ public class StockLevels extends ArrayList<Level>{
 		}
 		return puzzle3;
 	}
-
+	
+	/**
+	 * Generates Puzzle level 4
+	 * @return Puzzle Level 4
+	 */
 	public Level generatePuzzle4(){
 		puzzle4.setName("Puzzle4");
 		Probability p = new Probability();
@@ -139,6 +193,10 @@ public class StockLevels extends ArrayList<Level>{
 		return puzzle4;
 	}
 
+	/**
+	 * Generates Release level 1
+	 * @return Release Level 1
+	 */
 	public Level generateRelease1(){
 		release1.setName("Release1");
 		Probability p = new Probability();
@@ -182,7 +240,11 @@ public class StockLevels extends ArrayList<Level>{
 		release1.grid.getSquare(location5).setRelease(true);
 		return release1;
 	}
-
+	
+	/**
+	 * Generates Release level 2
+	 * @return Release Level 2
+	 */
 	public Level generateRelease2(){
 		release2.setName("Release2");
 		Probability p = new Probability();
@@ -227,6 +289,10 @@ public class StockLevels extends ArrayList<Level>{
 		return release2;
 	}
 
+	/**
+	 * Generates Release level 3
+	 * @return Release Level 3
+	 */
 	public Level generateRelease3(){
 		release3.setName("Release3");
 		Probability p = new Probability();
@@ -261,6 +327,10 @@ public class StockLevels extends ArrayList<Level>{
 		return release3;
 	}
 
+	/**
+	 * Generates Release level 4
+	 * @return Release Level 4
+	 */
 	public Level generateRelease4(){
 		release4.setName("Release4");
 		Probability p = new Probability();
@@ -296,6 +366,10 @@ public class StockLevels extends ArrayList<Level>{
 		return release4;
 	}
 
+	/**
+	 * Generates Elimination level 1
+	 * @return Elimination Level 1
+	 */
 	public Level generateElimination1(){
 		elimination1.setName("Elimination1");
 		Probability p = new Probability();
@@ -312,6 +386,11 @@ public class StockLevels extends ArrayList<Level>{
 		return elimination1;
 	}
 
+
+	/**
+	 * Generates Elimination level 2
+	 * @return Elimination Level 2
+	 */
 	public Level generateElimination2(){
 		elimination2.setName("Elimination2");
 		Probability p = new Probability();
@@ -328,6 +407,10 @@ public class StockLevels extends ArrayList<Level>{
 		return elimination2;
 	}
 
+	/**
+	 * Generates Elimination level 3
+	 * @return Elimination Level 3
+	 */
 	public Level generateElimination3(){
 		elimination3.setName("Elimination3");
 		Probability p = new Probability();
@@ -344,6 +427,10 @@ public class StockLevels extends ArrayList<Level>{
 		return elimination3;
 	}
 
+	/**
+	 * Generates Elimination level 4
+	 * @return Elimination Level 4
+	 */
 	public Level generateElimination4(){
 		elimination4.setName("Elimination4");
 		Probability p = new Probability();
@@ -359,6 +446,11 @@ public class StockLevels extends ArrayList<Level>{
 		}
 		return elimination4;
 	}
+
+	/**
+	 * Generates Lightning level 1
+	 * @return Lightning Level 1
+	 */
 	public Level generateLightning1(){
 		lightning1.setName("Lightning1");
 		Probability p = new Probability();
@@ -374,6 +466,10 @@ public class StockLevels extends ArrayList<Level>{
 		return lightning1;
 	}
 
+	/**
+	 * Generates Lightning level 2
+	 * @return Lightning Level 2
+	 */
 	public Level generateLightning2(){
 		lightning2.setName("Lightning2");
 		Probability p = new Probability();
@@ -390,6 +486,10 @@ public class StockLevels extends ArrayList<Level>{
 		return lightning2;
 	}
 
+	/**
+	 * Generates Lightning level 3
+	 * @return Lightning Level 3
+	 */
 	public Level generateLightning3(){
 		lightning3.setName("Lightning3");
 		Probability p = new Probability();
@@ -407,7 +507,10 @@ public class StockLevels extends ArrayList<Level>{
 		return lightning3;
 	}
 
-
+	/**
+	 * Generates Lightning level 4
+	 * @return Lightning Level 4
+	 */
 	public Level generateLightning4(){
 		lightning4.setName("Lightning4");
 		Probability p = new Probability();
@@ -425,7 +528,10 @@ public class StockLevels extends ArrayList<Level>{
 		return lightning4;
 	}
 
-
+	/**
+	 * Generates all 16 built in levels
+	 * @return a list of the 16 built in levels
+	 */
 	public ArrayList<Level>	generateLevels(){
 
 		generatePuzzle1();
@@ -471,20 +577,18 @@ public class StockLevels extends ArrayList<Level>{
 		return getLevels();
 	}
 
-
-
-
-
-
+	/**
+	 * Gets the list of levels
+	 * @return the list of levels
+	 */
 	public ArrayList<Level> getLevels() {
 		return levels;
 	}
 
-
-
-
-
-
+	/**
+	 * Sets the list of levels
+	 * @param levels the new list of levels
+	 */
 	public void setLevels(ArrayList<Level> levels) {
 		this.levels = levels;
 	}
